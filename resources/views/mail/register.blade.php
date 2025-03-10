6f70eeb8f9235a6b21813bb14eeb4fe0ca4c47e3
imzajjithvedha <zajjith@gmail.com>
2025-03-10 11:56:24 +0530

    register mail

---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,13 @@
+<p>Dear {{ $mail_data['name'] }},</p>
+
+<p>Welcome to GPNi! We're excited to have you on board.</p>
+
+<p>Your account is all set up, and you're ready to explore everything we have to offer.</p>
+
+<p>If you ever need help, our support team is here for you at info@thegpni.com. We can't wait for you to dive in and make the most of your experience!</p>
+
+<p>Happy exploring!</p>
+
+<p style="margin-top: 35px; margin-bottom: 3px;">Best regards,</p>
+
+<p style="margin-top: 0px;">GPNi</p>
