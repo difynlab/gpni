0a288fcb456a011f20d647973f8cf014168188cb
imzajjithvedha <zajjith@gmail.com>
2025-03-17 13:35:11 +0530

    profile

--- app/Http/Controllers/Backend/Course/CourseController.php
+++ app/Http/Controllers/Backend/Course/CourseController.php
@@ -11,7 +11,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Validation\Rule;
 
 class CourseController extends Controller
 {
