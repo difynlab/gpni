e6a18ba9b8ff3d28634d429716175a1177a02bfd
M.B. Sharqi Sajjath mohammath <90747178+MBSharqi@users.noreply.github.com>
2025-03-19 15:10:53 +0530

    Reminder mail task

---
+++ resources/views/mail/subscription-alert.blade.php
@@ -0,0 +1,9 @@
+<p>Dear {{ $user->name }},</p>
+ 
+<p>Your subscription will expire soon. Please renew your subscription to continue enjoying our services.</p>
+    
+<p>Happy exploring!</p>
+
+<p style="margin-top: 35px; margin-bottom: 3px;">Best regards,</p>
+
+<p style="margin-top: 0px;">GPNi</p>
