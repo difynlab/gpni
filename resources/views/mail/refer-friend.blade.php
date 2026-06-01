2d51fc161178a38b0596f08cd08cabc71a8b3971
Zajjith Vedha <zajjith@gmail.com>
2026-06-01 11:17:38 +0530

    advisory board and technical support chat font size

--- resources/views/mail/refer-friend.blade.php
+++ resources/views/mail/refer-friend.blade.php
@@ -1,8 +1,12 @@
 <p>Dear Sir/ Madam,</p>
 
-<p>{{ $mail_data['name'] }} has invited you to join our platform. We're looking forward to having you on board!</p>
+<p>{{ $mail_data['name'] }} has invited you to join the GPNi, Global Performance Nutrition Institute online education portal.</p>
 
-<p style="margin-bottom: 35px;">You can register your account by clicking this button. Please feel free to try out platform.</p>
+<p>GPNi® is the official partner for The ISSN. The world's first science-backed performance nutrition online education portal to be offered in multiple countries and available in multiple languages. You can choose GPNi to begin and advance in sports nutrition. </p>
+
+<p>We're looking forward to having you on board!</p>
+
+<p style="margin-bottom: 35px;">You can register your account by clicking this button. Please feel free to try out platform. </p>
 
 <a href="{{ route('frontend.register', $mail_data['code']) }}" style="background-color: #136fc9; color: white; padding: 10px 20px; font-size: 16px; text-decoration: none;">Join Here!</a>
 
